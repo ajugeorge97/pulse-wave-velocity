43bd4ded58297f50fb2381c90c51f661fa371a35
ajugeorge93 <40798642+ajugeorge93@users.noreply.github.com>
2018-07-03 19:36:09 +0530

    Add files via upload

---
+++ pulse_plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import csv
+x=[]
+y=[]
+with open('pulse_data','r') as csvfile:
+
+    plots=csv.reader(csvfile,delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))  #pulse from ist sensor
+        y.append(int(row[1]))  #pulse from second sensor
+
+print(x)
+print(y)
+iterations=len(x)  #we are ploting aganist iteration versus amplitude
+print("iterations:" + str(iterations))
+x_value=[]
+for i in range(iterations):
+    x_value.append(int(i+1))
+print(x_value)
+plt.subplot(221)
+plt.plot(x_value,y, label="pulse wave" )
+plt.title("Raw data",)
+plt.xlabel("Iterations")
+plt.ylabel("Amplitude")
+plt.show()
+
+
+
+
