5227c903ab6e514ef53b65dc0e26d18c29ba8914
ajugeorge93 <ajugeorge93@gmail.com>
2021-08-24 11:40:38 +0530

    filename correction

--- pulse_plot.py
+++ pulse_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 x=[]
 y=[]
-with open('pulse_data','r') as csvfile:
+with open('pulse_data.txt','r') as csvfile:
 
     plots=csv.reader(csvfile,delimiter=',')
     for row in plots:
